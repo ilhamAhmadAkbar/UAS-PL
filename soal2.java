bb43b128dde1561c05ab19ae6ae70ff7a7805c0b
miki253163 <mickysetiawan3@gmail.com>
2023-06-20 16:22:59 +0700

    nomor 02

--- soal2.java
+++ soal2.java
@@ -5,18 +5,17 @@ import java.io.InputStreamReader;
 public class soal2 {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         try {
             System.out.print("Masukkan teks: ");
             String text = reader.readLine();
 
             if (text.length() == 5) {
-                System.out.println("Panjang teks adalah 5 karakter.");
+                System.out.println("Panjang teks adalah 5 karakter");
             } else {
-                System.out.println("Panjang teks bukan 5 karakter.");
+                System.out.println("Panjang teks bukan 5 karakter");
             }
         } catch (IOException e) {
-            System.out.println("Terjadi kesalahan saat membaca input.");
+            System.out.println("Terjadi kesalahan saat membaca input");
             e.printStackTrace();
         } finally {
             try {
