cc56d60c0eebd34fd0cf4670423ac2e44170af61
miki253163 <mickysetiawan3@gmail.com>
2023-06-20 15:46:46 +0700

    nomor 01

--- soal1.java
+++ soal1.java
@@ -1,15 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class soal1 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan angka: ");
-        int angka = input.nextInt();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        if (angka % 5 == 0) {
-            System.out.println(angka + " adalah kelipatan 5");
-        } else {
-            System.out.println(angka + " bukan kelipatan 5");
+        System.out.print("Masukkan sebuah angka: ");
+
+        try {
+            int angka = Integer.parseInt(reader.readLine());
+
+            if (angka % 5 == 0) {
+                System.out.println("Angka " + angka + " adalah kelipatan 5.");
+            } else {
+                System.out.println("Angka " + angka + " bukan kelipatan 5.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Input tidak valid! Input harus berupa angka.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
